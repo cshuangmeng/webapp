73d74c42a68d324541bcc22c4f3a3ab9b7728619
zhoujunxia <zhoujunxia@haotang365.com.cn>
2017-07-11 21:17:43 +0800

    购物车+订单确认+下单

--- pc/js/goodsDetail.js
+++ pc/js/goodsDetail.js
@@ -2,8 +2,6 @@ $(function(){
 	var $more = $('#js-more'),
 		$moreList = $('#js-moreList');
 	
-	var config = {};
-	
 	//显示隐藏更多
 	$more.mouseenter(function(){
 		$moreList.css("display","block");
@@ -49,57 +47,6 @@ $(function(){
 			_this.prev().val(val);
 		});
 	}
-	
-	//加载商品详情(id=1)
-	$.ajax({
-		type: "POST",
-	    url: goodsinfo,
-	    dataType:"json",
-	    data:{"id":config.goodId,"uuid":config.uuid,"openId":config.openid},
-	    async:false,
-		success:function(data){
-			if(data.code == 0){
-				config.goodId = data.data.id;
-				$goshop.attr("shopId",data.data.shopId);
-				if(data.data.infoImgs){
-					var infoImgsArr = new Array();  
-					var infoImgsList = data.data.infoImgs;
-					infoImgsArr = infoImgsList.split(',');
-					for(var i=0;i<infoImgsArr.length;i++){
-						$banner.append('<div class="item"><img src="'+infoImgsArr[i]+'" alt=""></div>');
-					}
-					$('.item:eq(1)').addClass('active');
-					$('.item:eq(0)').remove();
-					$('.carousel').carousel();
-				}
-				
-				if(data.data.details){
-					var detailsArr = eval(data.data.details);
-					for(var i=0;i<detailsArr.length;i++){
-						$canshulist.append('<p class="li"><b>'+detailsArr[i].label+':</b><span>'+detailsArr[i].value+'</span></p>');
-					}
-				}
-				if(data.data.detailImgs){
-					var detailImgsArr = new Array();  
-					var detailImgsList = data.data.detailImgs;
-					detailImgsArr = detailImgsList.split(',');
-					for(var i=0;i<detailImgsArr.length;i++){
-						$detailImgs.append('<img src="'+detailImgsArr[i]+'"/>');
-					}
-				}
-				
-				$goodName.text(data.data.name);
-				$price.text(data.data.price);
-				//extras
-				$lowPrice.text(data.data.extras.miniPrice);
-				$isFree.text(data.data.extras.freight);
-				$sales.text(data.data.extras.sellAmount);
-				$address.text(data.data.extras.goodsArea);
-				$buluofen.text(data.data.extras.backPoint);
-			}
-		}
-	});
-	
 	
 	
 });
